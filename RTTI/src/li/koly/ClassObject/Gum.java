89ddb32e1f1ef9cea47b8bb45d7ddeac25929085
koly <me.koly.li@gmail.com>
2012-11-19 08:13:54 +0800

    Finally made the test pass, learn how to use the invoke() method, and use the 'li.koly.ClassObject.Gum' not just 'Gum', need to add the package name

--- RTTI/src/li/koly/ClassObject/Gum.java
+++ RTTI/src/li/koly/ClassObject/Gum.java
@@ -1,16 +1,13 @@
 package li.koly.ClassObject;
 
 public class Gum {
-//    private static String i;
-//
-//    static{
-//        i = "Loading Gum!";
-//    }
-//
-//    public String getGum(){
-//        return i;
-//    }
-    static {
-    System.out.println("Loading Gum!");
-}
+    private static String i;
+
+    static{
+        i = "Loading Gum!";
+    }
+
+    public String getGum(){
+        return i;
+    }
 }
