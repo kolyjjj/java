1527459bcfeb3d6b7d2b9201e5b379a23b293ffc
kolyjjj <me.koly.li@gmail.com>
2013-11-01 21:21:31 +0800

    change the parameter name

--- refactor/src/main/java/li/koly/ReplaceDataValueWithObject/Order.java
+++ refactor/src/main/java/li/koly/ReplaceDataValueWithObject/Order.java
@@ -4,15 +4,15 @@ public class Order {
 
     private Customer customer;
 
-    public Order(String customer){
-        this.customer = new Customer(customer);
+    public Order(String customerName){
+        this.customer = new Customer(customerName);
     }
 
     public String getCustomer(){
         return customer.getName();
     }
 
-    public void setCustomer(String customer){
-        this.customer = new Customer(customer);
+    public void setCustomer(String customerName){
+        this.customer = new Customer(customerName);
     }
 }
