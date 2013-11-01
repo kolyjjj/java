3b58eaae1a79adafe639e7e2a81ac628f253114c
kolyjjj <me.koly.li@gmail.com>
2013-11-01 21:34:58 +0800

    change Value to Reference - create dictionary

--- refactor/src/main/java/li/koly/ReplaceDataValueWithObject/Customer.java
+++ refactor/src/main/java/li/koly/ReplaceDataValueWithObject/Customer.java
@@ -1,7 +1,21 @@
 package li.koly.ReplaceDataValueWithObject;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Customer {
     final String name;
+    private static Dictionary instances = new Hashtable();
+
+    private static void loadCustomers(){
+        new Customer("Lemon Car Hire").store();
+        new Customer("Associated Coffee Machines").store();
+        new Customer("Bilston Gasworks").store();
+    }
+
+    private void store() {
+        instances.put(this.name, this);
+    }
 
     private Customer(String name){
         this.name = name;
