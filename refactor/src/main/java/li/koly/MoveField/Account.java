efb54ff829077bc4cd4138524d0473fb3599eee3
kolyjjj <me.koly.li@gmail.com>
2013-11-05 21:45:18 +0800

    [Move Field] self encapsulation when many methods use the interest attribute

--- refactor/src/main/java/li/koly/MoveField/Account.java
+++ refactor/src/main/java/li/koly/MoveField/Account.java
@@ -2,8 +2,17 @@ package li.koly.MoveField;
 
 public class Account {
     private AccountType type;
+    private double interest;
 
     double interestForAmount_days(double amount, int days){
-        return type.getInterestRate() * amount * days / 365;
+        return getInterest() * amount * days / 365;
+    }
+
+    public double getInterest() {
+        return interest;
+    }
+
+    public void setInterest(double interest) {
+        this.interest = interest;
     }
 }
