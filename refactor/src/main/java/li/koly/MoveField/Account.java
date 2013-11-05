6faf8395c3d2cd07fde30840cb473f68efa5fa04
kolyjjj <me.koly.li@gmail.com>
2013-11-05 21:47:03 +0800

    [Move Field] delete and inline the getter and setter

--- refactor/src/main/java/li/koly/MoveField/Account.java
+++ refactor/src/main/java/li/koly/MoveField/Account.java
@@ -2,17 +2,9 @@ package li.koly.MoveField;
 
 public class Account {
     private AccountType type;
-    private double interest;
 
     double interestForAmount_days(double amount, int days){
-        return getInterest() * amount * days / 365;
+        return type.getInterestRate() * amount * days / 365;
     }
 
-    public double getInterest() {
-        return type.getInterestRate();
-    }
-
-    public void setInterest(double interest) {
-        type.setInterestRate(interest);
-    }
 }
