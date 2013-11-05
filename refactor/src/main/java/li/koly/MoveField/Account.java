19864ee8cfef914085a83d79e9e3aa45387455d0
kolyjjj <me.koly.li@gmail.com>
2013-11-05 21:44:09 +0800

    [Move Field] delete interest in Account class

--- refactor/src/main/java/li/koly/MoveField/Account.java
+++ refactor/src/main/java/li/koly/MoveField/Account.java
@@ -2,7 +2,6 @@ package li.koly.MoveField;
 
 public class Account {
     private AccountType type;
-    private double interest;
 
     double interestForAmount_days(double amount, int days){
         return type.getInterestRate() * amount * days / 365;
