38a9b54fbc4515128e796acc4bf8a98b56d90aaf
kolyjjj <me.koly.li@gmail.com>
2013-11-09 09:31:32 +0800

    [Extract Superclass] pull down fields

--- refactor/src/main/java/li/koly/ExtractSubclass/LaborItem.java
+++ refactor/src/main/java/li/koly/ExtractSubclass/LaborItem.java
@@ -5,6 +5,7 @@ public class LaborItem extends JobItem {
 
     public LaborItem(int quantity, Employee employee) {
         super(0, quantity, employee, true);
+        this.employee = employee;
     }
 
 
