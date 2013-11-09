d5d7ddea241a878107d59c95f94d379839992066
kolyjjj <me.koly.li@gmail.com>
2013-11-09 08:36:50 +0800

    [Extract Superclass] pull up fields and methods, why methods?

--- refactor/src/main/java/li/koly/ExtractSuperclass/Employee.java
+++ refactor/src/main/java/li/koly/ExtractSuperclass/Employee.java
@@ -2,18 +2,13 @@ package li.koly.ExtractSuperclass;
 
 public class Employee extends Party{
 
-    private final String name;
     private final String id;
     private final int annualCost;
 
     public Employee(String name, String id, int annualCost) {
-        this.name = name;
+        super(name);
         this.id = id;
         this.annualCost = annualCost;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getId() {
--- refactor/src/main/java/li/koly/ExtractSuperclass/Party.java
+++ refactor/src/main/java/li/koly/ExtractSuperclass/Party.java
@@ -1,4 +1,13 @@
 package li.koly.ExtractSuperclass;
 
 public abstract class Party {
+    protected final String name;
+
+    public Party(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
