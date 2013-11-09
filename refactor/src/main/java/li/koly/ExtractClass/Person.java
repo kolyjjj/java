ef47b5a359a5190e8ee7c7eb883e80849ae7326c
kolyjjj <me.koly.li@gmail.com>
2013-11-09 10:44:10 +0800

    [Extract Superclass] delete unused fields

--- refactor/src/main/java/li/koly/ExtractClass/Person.java
+++ refactor/src/main/java/li/koly/ExtractClass/Person.java
@@ -3,8 +3,6 @@ package li.koly.ExtractClass;
 public class Person {
 
     private String name;
-    private String officeAreaCode;
-    private String officeNumber;
     private TelephoneNumber telephoneNumber = new TelephoneNumber();
 
     public String getName() {
@@ -17,13 +15,5 @@ public class Person {
 
     public String getOfficeNumber() {
         return telephoneNumber.getOfficeNumber();
-    }
-
-    public void setOfficeAreaCode(String officeAreaCode) {
-        this.officeAreaCode = officeAreaCode;
-    }
-
-    public void setOfficeNumber(String officeNumber) {
-        this.officeNumber = officeNumber;
     }
 }
