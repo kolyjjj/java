350d5641d56a8a87854ca1be66ebfd1f31e4d3ac
kolyjjj <me.koly.li@gmail.com>
2013-11-21 17:52:39 +0800

    [use function objects] simple version

---
+++ refactor/src/main/java/li/koly/EffectiveJava/useFunctionObjectsToRepresentStrategies/StringLengthComparator.java
@@ -0,0 +1,7 @@
+package li.koly.EffectiveJava.useFunctionObjectsToRepresentStrategies;
+
+public class StringLengthComparator {
+    public int compare(String s1, String s2){
+        return s1.length() - s2.length();
+    }
+}
