2f94431e1867691fe6dbd381d245d4977447c401
kolyjjj <me.koly.li@gmail.com>
2013-11-21 17:54:08 +0800

    [use function objects] use static final to hold the instance

--- refactor/src/main/java/li/koly/EffectiveJava/useFunctionObjectsToRepresentStrategies/StringLengthComparator.java
+++ refactor/src/main/java/li/koly/EffectiveJava/useFunctionObjectsToRepresentStrategies/StringLengthComparator.java
@@ -1,6 +1,13 @@
 package li.koly.EffectiveJava.useFunctionObjectsToRepresentStrategies;
 
 public class StringLengthComparator {
+
+    private StringLengthComparator(){
+
+    }
+
+    public static final StringLengthComparator stringLengthComparator = new StringLengthComparator();
+
     public int compare(String s1, String s2){
         return s1.length() - s2.length();
     }
